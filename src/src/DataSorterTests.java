6e29224906ae37f9440eff0275259121d959237a
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-10-12 10:38:44 +0300

    removed main method and removed static modifier

--- src/src/DataSorterTests.java
+++ src/src/DataSorterTests.java
@@ -11,13 +11,15 @@ public class DataSorterTests {
 
 	@Test
 	void testSortDates() {
+		DateSorter dateSorter = new DateSorter();
+
 		List<LocalDate> unsortedDates = new ArrayList<>();
 		unsortedDates.add(LocalDate.of(2004, 7, 1));
 		unsortedDates.add(LocalDate.of(2005, 1, 2));
 		unsortedDates.add(LocalDate.of(2007, 1, 1));
 		unsortedDates.add(LocalDate.of(2032, 5, 3));
 
-		Collection<LocalDate> sortedDates = DateSorter.sortDates(unsortedDates);
+		Collection<LocalDate> sortedDates = dateSorter.sortDates(unsortedDates);
 
 		List<LocalDate> expectedSortedDates = new ArrayList<>();
 		expectedSortedDates.add(LocalDate.of(2005, 1, 2));
--- src/src/DateSorter.java
+++ src/src/DateSorter.java
@@ -1,13 +1,11 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DateSorter {
 
-	public static Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+	public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
 		List<LocalDate> datesMonthR = unsortedDates.stream()
 				.filter(date -> date.getMonth().toString().toLowerCase().contains("r"))
 				.sorted((date1, date2) -> date1.getMonth().compareTo(date2.getMonth()))
@@ -21,15 +19,5 @@ public class DateSorter {
 		datesMonthR.addAll(datesMonthNoR);
 
 		return datesMonthR;
-	}
-
-	public static void main(String [] args) {
-		List<LocalDate> dates = new ArrayList<>();
-		dates.add(LocalDate.of(2022, 6, 1));
-		dates.add(LocalDate.of(2022, 5, 1));
-		dates.add(LocalDate.of(2025, 7, 1));
-		dates.add(LocalDate.of(2022, 8, 1));
-
-		System.out.println(sortDates(dates));
 	}
 }
